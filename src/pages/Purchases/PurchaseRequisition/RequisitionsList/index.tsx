584316196f7db04b201212d7eeb2cd5741c57400
HardBoris <bgaibor03@gmail.com>
2023-12-06 22:12:23 -0400

    feat: tabela iniciada

--- src/pages/Purchases/PurchaseRequisition/RequisitionsList/index.tsx
+++ src/pages/Purchases/PurchaseRequisition/RequisitionsList/index.tsx
@@ -5,11 +5,17 @@ export const RequisitionsList = () => {
   const { solicitudes } = usePurchaseRequest();
   return (
     <>
-      <div className="faja-de-titulos"></div>
+      <div className="faja-de-titulos">
+        <div className="titulos">
+          <div className="id_titulo">id</div>
+          <div className="date_titulo">fecha</div>
+          <div className="btn_titulo"></div>
+        </div>
+      </div>
       <div className="tabla-de-solicitudes">
         {solicitudes &&
           solicitudes.map((item, index) => (
-            <div key={index}>
+            <div key={index} className="request-row">
               <div>{item.purchaseRequestId}</div>
               <div>{item.purchaseRequestDate}</div>
               <div>converter</div>
